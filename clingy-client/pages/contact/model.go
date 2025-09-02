084227ac0df9319785fc898d36659e21d842b599
Landro3 <andrew.landis3@gmail.com>
2025-09-01 20:52:52 -0400

    added creating contacts and setting chat user

--- clingy-client/pages/contact/model.go
+++ clingy-client/pages/contact/model.go
@@ -1,6 +1,8 @@
 package contact
 
 import (
+	"clingy-client/services"
+
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -15,21 +17,26 @@ const (
 	Remove
 )
 
+type SelectChatMsg struct {
+	Chat services.ContactInfo
+}
+
+func SelectChatCmd(contactInfo services.ContactInfo) func() tea.Msg {
+	return func() tea.Msg {
+		return SelectChatMsg{contactInfo}
+	}
+}
+
 type Model struct {
 	list       list.Model
-	contacts   []list.Item
 	inputs     []textinput.Model
 	focusIndex int
 	mode       ModeType
 	ready      bool
+	contact    *services.Contact
 }
 
-func InitialModel() Model {
-	items := []list.Item{
-		user{name: "John Nathan", id: "fjw38fhfn9v87fga9jifnga0"},
-		user{name: "Alex Zander", id: "0q9fhva73098ang0b9uahfa"},
-	}
-
+func InitialModel(contactService *services.Contact) Model {
 	keys := list.DefaultKeyMap()
 	keys.GoToStart.SetEnabled(false)
 	keys.GoToEnd.SetEnabled(false)
@@ -37,7 +44,8 @@ func InitialModel() Model {
 	keys.PrevPage.SetEnabled(false)
 	keys.Quit.SetEnabled(false)
 
-	contactList := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	listItems := contactService.ToListItems()
+	contactList := list.New(listItems, list.NewDefaultDelegate(), 0, 0)
 	contactList.KeyMap = keys
 	contactList.SetShowHelp(false)
 
@@ -46,11 +54,11 @@ func InitialModel() Model {
 	inputs := []textinput.Model{usernameInput, uuidInput}
 
 	m := Model{
-		list:     contactList,
-		ready:    false,
-		contacts: items,
-		inputs:   inputs,
-		mode:     None,
+		list:    contactList,
+		ready:   false,
+		inputs:  inputs,
+		mode:    None,
+		contact: contactService,
 	}
 
 	m.list.Title = "Contacts"
