ed5be0f1edeeaf763e7a6cad4b73ebabd73b6173
Landro3 <andrew.landis3@gmail.com>
2025-12-30 18:06:28 -0500

    removed logs

--- clingy-client/api/handlers/chat.go
+++ clingy-client/api/handlers/chat.go
@@ -60,30 +60,30 @@ func (h *ChatHandler) GetMessageStream(w http.ResponseWriter, r *http.Request) {
 	}
 
 	messageChan := services.GetMessageChannel()
-	util.Log("🔥 STREAM: Got message channel, starting loop")
 
 	for {
 		select {
 		case msg, ok := <-messageChan:
 			if !ok {
-				util.Log("🔥 STREAM: Channel closed")
+				util.Log("STREAM: Channel closed")
 				return
 			}
-			util.Log(fmt.Sprintf("🔥 STREAM: Received message from channel - From: %s, To: %s, Message: %s", msg.From, msg.To, msg.Message))
+
 			data, err := json.Marshal(msg)
 			if err != nil {
-				util.Log(fmt.Sprintf("🔥 STREAM: Error marshaling message: %v", err))
+				util.Log(fmt.Sprintf("STREAM: Error marshaling message: %v", err))
 				continue
 			}
+
 			_, err = fmt.Fprintf(w, "data: %s\n\n", string(data))
 			if err != nil {
-				util.Log(fmt.Sprintf("🔥 STREAM: Error writing to response: %v", err))
+				util.Log(fmt.Sprintf("STREAM: Error writing to response: %v", err))
 				return
 			}
 			w.(http.Flusher).Flush()
-			util.Log(fmt.Sprintf("🔥 STREAM: ✅ Successfully wrote to stream: %s", string(data)))
+			util.Log(fmt.Sprintf("STREAM: ✅ Successfully wrote to stream: %s", string(data)))
 		case <-r.Context().Done():
-			util.Log("🔥 STREAM: Client disconnected")
+			util.Log("STREAM: Client disconnected")
 			return
 		}
 	}
