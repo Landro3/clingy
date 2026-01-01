10d152fdc79096e92de7ed3ff15143c2b94d162a
Landro3 <andrew.landis3@gmail.com>
2026-01-01 14:52:32 -0500

    connect to server on open

--- clingy-client/ui/src/pages/intro.tsx
+++ clingy-client/ui/src/pages/intro.tsx
@@ -1,22 +1,38 @@
 import { TextAttributes } from '@opentui/core';
 import { useKeyboard } from '@opentui/react';
 import { Pages, useNavigation } from '../context/navigation';
+import { type ServerConfig, getServerConfig, setServerConfig as setServerConfigApi } from '../api/config';
+import { useMutation, useQuery } from '../hooks/api';
+import { useEffect } from 'react';
 
 export default function Intro() {
   const { navigate } = useNavigation();
 
-  useKeyboard((key) => {
-    if (key.name === 'return') {
+  const { data: serverConfig, loading: loadingServerConfig, refetch } = useQuery<ServerConfig>(getServerConfig);
+  const { mutate: setServerConfig } = useMutation(setServerConfigApi);
+
+  // TODO: Modal on error here
+  useEffect(() => {
+    if (loadingServerConfig) return;
+    if (serverConfig) {
+      setServerConfig({
+        username: serverConfig.username,
+        serverAddr: serverConfig. serverAddr,
+      }).then(() => navigate(Pages.Chat));
+      return;
+    }
+  
+    if (!serverConfig && !loadingServerConfig) {
       navigate(Pages.Config);
     }
-  });
+  }, [serverConfig]);
 
   return (
     <box alignItems="center" justifyContent="center" flexGrow={1}>
       <box justifyContent="center" alignItems="flex-start">
         <ascii-font font="tiny" text="clingy" />
         <text attributes={TextAttributes.DIM}>E2E Encrypted & AI-Powered Terminal Messaging</text>
-        <text attributes={TextAttributes.DIM}>Make this load the config and if not exists then go there</text>
+        <text attributes={TextAttributes.DIM}>connecting to server...</text>
       </box>
     </box>
   );
