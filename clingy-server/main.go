c3d1e2eb47ec1516327ed288f08a5cb1135dbfda
Landro3 <andrew.landis3@gmail.com>
2026-03-10 21:25:12 -0400

    separated server startup

--- clingy-server/main.go
+++ clingy-server/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"log"
 	"clingy-server/services"
 )
 
 func main() {
-	serverType := services.Http2 // TODO: get from cli
-	server := services.NewClingyServer(serverType) // TODO: add connection details like port, host
-	err := server.ListenAndServeTLS("", "")
-	if err != nil {
-		log.Fatal(err)
-	}
+	serverType := services.Http3 // TODO: get from cli
+	services.StartClingyServer(serverType) // TODO: add connection details like port, host
 }
 
--- clingy-server/services/server.go
+++ clingy-server/services/server.go
@@ -1,15 +1,14 @@
 package services
 
 import (
+	"clingy-server/util"
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
 	"github.com/quic-go/quic-go"
 	"github.com/quic-go/quic-go/http3"
-	"context"
-	"clingy-server/util"
+	"log"
+	"net/http"
 )
 
 type registerBody struct {
@@ -29,12 +28,8 @@ type chatMessage struct {
 	Message string
 }
 
-type Server interface {
-	ListenAndServeTLS(certFile, keyFile string) error
-	Shutdown(ctx context.Context) error
-}
-
 type ServerType int
+
 const (
 	Http2 ServerType = iota
 	Http3
@@ -44,40 +39,29 @@ type ClingyServer struct {
 	serverType ServerType
 	mux        *http.ServeMux
 	connMap    *ConnectionMap
-	server     Server 
+	certFile	 string
+	keyFile    string
 }
 
-
-func NewClingyServer(serverType ServerType) Server {
-  cs := &ClingyServer{
+func StartClingyServer(serverType ServerType) {
+	cs := &ClingyServer{
 		serverType: serverType,
-		mux:        http.NewServeMux(),
-		connMap:    NewConnectionMap(),
+		mux: http.NewServeMux(),
+		connMap: NewConnectionMap(),
+		certFile: "services/server.crt",
+		keyFile: "services/server.key",
 	}
 
 	cs.mux.HandleFunc("POST /register", cs.register)
 	cs.mux.HandleFunc("POST /chat", cs.chat)
 
-	tlsConfig := cs.getTlsConfig()
+
 
 	if serverType == Http2 {
-		server := &http.Server{
-			Handler:   cs.mux,
-			Addr:      "0.0.0.0:8443",
-			TLSConfig: tlsConfig,
-		}
-
-		return server
+		cs.startHttp2Server()
+	} else {
+		cs.startHttp3Server()
 	}
-
-	server := &http3.Server{
-		Handler:    cs.mux,
-		Addr:       "0.0.0.0:8443",
-		TLSConfig:  http3.ConfigureTLSConfig(tlsConfig),
-		QUICConfig: &quic.Config{},
-	}
-
-	return server
 }
 
 func (cs *ClingyServer) register(w http.ResponseWriter, r *http.Request) {
@@ -105,15 +89,9 @@ func (cs *ClingyServer) register(w http.ResponseWriter, r *http.Request) {
 		Message:    "Registration successful",
 	}
 
-	responseBytes, err := json.Marshal(response)
-	if err != nil {
-		log.Printf("Error marshaling registration response: %v", err)
+	if err := cs.flushSSEMessage(w, response); err != nil {
+		log.Printf("Error sending registration response: %v", err)
 		return
-	}
-
-	fmt.Fprintf(w, "data: %s\n\n", string(responseBytes))
-	if flusher, ok := w.(http.Flusher); ok {
-		flusher.Flush()
 	}
 
 	// TODO: change to UUID
@@ -136,43 +114,80 @@ func (cs *ClingyServer) chat(w http.ResponseWriter, r *http.Request) {
 
 	writer, exists := cs.connMap.Get(body.To)
 	if exists {
-		jsonBytes, _ := json.Marshal(body)
-		_, err := fmt.Fprintf(writer, "data: %s\n\n", string(jsonBytes))
-		if err != nil {
-			log.Printf("STREAM: Error writing to response: %v", err)
+		if err := cs.flushSSEMessage(writer, body); err != nil {
+			log.Printf("STREAM: Error sending chat message: %v", err)
 			return
-		}
-		if flusher, ok := writer.(http.Flusher); ok {
-			flusher.Flush()
 		}
 		log.Printf("✅ Sent message to %s", body.To)
 	} else {
 		log.Printf("User %s not connected", body.To)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-
 	cs.connMap.LogConnections()
 }
 
-func (cs *ClingyServer) getTlsConfig() *tls.Config {
-	cert, err := tls.LoadX509KeyPair("server.crt", "server.key")
+func (cs *ClingyServer) flushSSEMessage(w http.ResponseWriter, data interface{}) error {
+	jsonBytes, err := json.Marshal(data)
 	if err != nil {
+		return fmt.Errorf("error marshaling data: %v", err)
+	}
+
+	_, err = fmt.Fprintf(w, "data: %s\n\n", string(jsonBytes))
+	if err != nil {
+		return fmt.Errorf("error writing to response: %v", err)
+	}
+
+	if flusher, ok := w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+
+	return nil
+}
+
+func (cs *ClingyServer) startHttp2Server() {
+	cert, err := tls.LoadX509KeyPair("services/server.crt", "services/server.key")
+	if err != nil {
+		log.Fatal("Error loading HTTP2 cert")
 		log.Fatal(err)
 	}
 
-	if (cs.serverType == Http2) {
-		return &tls.Config{
-			MinVersion:   tls.VersionTLS12,
-			Certificates: []tls.Certificate{cert},
-			NextProtos:   []string{"h2", "http/1.1"},
-		}
+	tlsConfig := &tls.Config{
+		MinVersion:   tls.VersionTLS12,
+		Certificates: []tls.Certificate{cert},
+		NextProtos:   []string{"h2", "http/1.1"},
 	}
 
-	return &tls.Config{
+	server := &http.Server{
+		Handler: cs.mux,
+		Addr: "localhost:8443",
+		TLSConfig: tlsConfig,
+	}
+
+	log.Printf("HTTP2 server listening at %s", server.Addr)
+	server.ListenAndServe()
+}
+
+func (cs*ClingyServer) startHttp3Server() {
+	cert, err := tls.LoadX509KeyPair(cs.certFile, cs.keyFile)
+	if err != nil {
+		log.Fatal("Error loading HTTP3 cert")
+		log.Fatal(err)
+		return
+	}
+
+	tlsConfig := &tls.Config{
 		MinVersion:   tls.VersionTLS13,
 		Certificates: []tls.Certificate{cert},
 		NextProtos:   []string{"h3"},
 	}
-}
 
+	server := &http3.Server{
+		Handler:    cs.mux,
+		Addr:       "localhost:8443",
+		TLSConfig:  http3.ConfigureTLSConfig(tlsConfig),
+		QUICConfig: &quic.Config{},
+	}
+
+	log.Printf("HTTP3 server listening at %s", server.Addr)
+	server.ListenAndServe()
+}
